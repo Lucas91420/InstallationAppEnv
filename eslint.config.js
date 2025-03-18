b9253c06c2d72d6716f9e57a5ec5a51a57af9a13
Lucas <lukasvarsavaux@gmail.com>
2025-03-18 08:53:29 +0100

    feat: Create new button

--- eslint.config.js
+++ eslint.config.js
@@ -9,8 +9,8 @@ module.exports = [
     languageOptions: { 
       globals: {
         ...globals.browser,
-        ...globals.node,   // Supporte Node.js (`require`, `module`, `__dirname`)
-        ...globals.jest    // Supporte Jest (`test`, `expect`)
+        ...globals.node,  
+        ...globals.jest    
       },
       ecmaVersion: "latest",
       sourceType: "module"
@@ -19,14 +19,14 @@ module.exports = [
   pluginJs.configs.recommended,
   {
     plugins: {
-      react: pluginReact,  // Utilisation correcte du plugin React
-      jest: pluginJest     // Utilisation correcte du plugin Jest
+      react: pluginReact, 
+      jest: pluginJest    
     }
   },
   {
     rules: {
-      "react/react-in-jsx-scope": "off", // Corrige JSX `<App />`
-      "no-redeclare": "warn",  // Réduit la sévérité des variables redéclarées
+      "react/react-in-jsx-scope": "off",
+      "no-redeclare": "warn",  
       "no-undef": "error",
       "no-prototype-builtins": "off"
     }
